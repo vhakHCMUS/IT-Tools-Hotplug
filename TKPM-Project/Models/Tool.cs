9d7f1e22a59fa851987357131d56fdf00510aa12
kindinh903 <kindinh903@gmail.com>
2025-04-02 04:00:02 +0700

    update tool model

--- TKPM-Project/Models/Tool.cs
+++ TKPM-Project/Models/Tool.cs
@@ -1,7 +1,19 @@
-﻿public class Tool
+﻿using TKPM_Project.Models.Tools;
+
+public class Tool : ITool
 {
     public int Id { get; set; }
-    public string Name { get; set; }
-    public string Description { get; set; }
+    public string Name { get; set; } = "Any tool";
+    public string Description { get; set; } = "No description";
+    public bool IsPremium { get; set; } = false;
+    public string Category { get; set; } = "General";
+    public bool IsAvailable { get; set; } = true;
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public async Task<object> ExecuteAsync(params object[] inputs)
+    {
+        return await Task.FromResult(new { Message = "Execution completed" });
+    }
+
+    public string CustomViewTemplate { get; set; }
+    public void Dispose() { }
 }
